7f6855848957afddc616bf5c96b20f915a9ca4ae
Monkpy <34485630+Monkpy@users.noreply.github.com>
2020-02-27 15:49:39 +0800

    Update Bili.py

--- Bili.py
+++ Bili.py
@@ -21,7 +21,7 @@ class Bilib(object):
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.67 Safari/537.36'
         }
 
-        # 下载视频请求头--不知道为什么要加
+        # 下载视频请求头
         self.downloadVideoHeaders = {
             'Origin': 'https://www.bilibili.com',
             'Referer': 'https://www.bilibili.com/video/av24546213',
